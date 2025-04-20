6ac46793a175b7d5635bef9d5b3dcea7ae8020a7
Chris Diebold <chrisdiebold@protonmail.com>
2025-04-20 15:21:22 -0400

    adding icons and additional settings

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -11,3 +11,10 @@ vim.keymap.set(
   end,
   { silent = true, buffer = bufnr }
 )
+
+-- Highlight rust todo!().
+vim.b.minihipatterns_config = {
+  highlighters = {
+    rust_todo = { pattern = "todo!", group = "MiniHipatternsTodo" },
+  },
+}
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,5 @@
+local arrows = require('icons').arrows
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
@@ -23,13 +25,37 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- Show whitespace.
+vim.opt.list = true
+vim.opt.listchars = { space = '⋅', trail = '⋅', tab = '  ↦' }
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+    vim.opt.clipboard = 'unnamedplus'
 end)
+
+-- Disable horizontal scrolling.
+vim.o.mousescroll = 'ver:3,hor:0'
+
+-- Wrap long lines at words.
+vim.o.linebreak = true
+
+-- Folding.
+vim.o.foldcolumn = '1'
+vim.o.foldlevelstart = 99
+vim.wo.foldtext = ''
+
+-- UI characters.
+vim.opt.fillchars = {
+    eob = ' ',
+    fold = ' ',
+    foldclose = arrows.right,
+    foldopen = arrows.down,
+    foldsep = ' ',
+    msgsep = '─',
+}
 
 -- Enable break indent
 vim.opt.breakindent = true
@@ -46,27 +72,34 @@ vim.opt.smartcase = true
 -- Keep signcolumn on by default
 vim.opt.signcolumn = 'yes'
 
--- Decrease update time
-vim.opt.updatetime = 250
+-- Update times and timeouts.
+vim.o.updatetime = 300
+vim.o.timeoutlen = 500
+vim.o.ttimeoutlen = 10
 
--- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
+-- Completion.
+vim.opt.wildignore:append { '.DS_Store' }
+vim.o.completeopt = 'menuone,noselect,noinsert'
+vim.o.pumheight = 15
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
+
+-- Diff mode settings.
+-- Setting the context to a very large number disables folding.
+vim.opt.diffopt:append 'vertical,context:99'
+
+vim.opt.shortmess:append {
+    w = true,
+    s = true,
+}
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
@@ -78,3 +111,15 @@ vim.opt.confirm = true
 
 vim.opt.undofile = true
 
+-- Status line.
+vim.o.laststatus = 3
+vim.o.cmdheight = 1
+
+-- Disable cursor blinking in terminal mode.
+vim.o.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,t:block-TermCursor'
+
+-- Disable health checks for these providers.
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
