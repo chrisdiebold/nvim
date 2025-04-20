98f280a09e5df31712dd6164ca1cf90a1147db27
Chris Diebold <chrisdiebold@protonmail.com>
2025-04-20 17:48:15 -0400

    setup java with java 24

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,1 +1,31 @@
-return { "nvim-java/nvim-java", opts = {} }
+return {
+    'nvim-java/nvim-java',
+    dependencies = {
+        {
+            'neovim/nvim-lspconfig',
+            opts = {
+                servers = {
+                    jdtls = {
+                        settings = {
+                            java = {
+                                configuration = {
+                                    runtimes = {
+                                        {
+                                            name = 'JavaSE-24',
+                                            path = '/Users/chris/.sdkman/candidates/java/current/bin/java',
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            setup = {
+                jdtls = function()
+                    require('java').setup {}
+                end,
+            },
+        },
+    },
+}
