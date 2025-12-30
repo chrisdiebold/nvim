56dfe54baa896ad1b03c73b5431a688267207f22
Chris Diebold <chrisdiebold@protonmail.com>
2025-12-29 21:53:56 -0500

    fixed lua snip and added icons in completion plugin

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,45 +1,29 @@
+kind_icons = require('icons').symbol_kinds
+
 return { -- Autocompletion
     'saghen/blink.cmp',
 
     dependencies = {
-        'rafamadriz/friendly-snippets',
+        { 'L3MON4D3/LuaSnip', version = 'v2.*' },
         'folke/lazydev.nvim',
     },
-    version = '1.*',
+    build = 'cargo +nightly build --release',
+    event = 'InsertEnter',
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
     opts = {
         keymap = {
-            -- 'default' (recommended) for mappings similar to built-in completions
-            --   <c-y> to accept ([y]es) the completion.
-            --    This will auto-import if your LSP supports it.
-            --    This will expand snippets if the LSP sent a snippet.
-            -- 'super-tab' for tab to accept
-            -- 'enter' for enter to accept
-            -- 'none' for no mappings
-            --
-            -- For an understanding of why the 'default' preset is recommended,
-            -- you will need to read `:help ins-completion`
-            --
-            -- No, but seriously. Please read `:help ins-completion`, it is really good!
-            --
-            -- All presets have the following mappings:
-            -- <tab>/<s-tab>: move to right/left of your snippet expansion
-            -- <c-space>: Open menu or open docs if already open
-            -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-            -- <c-e>: Hide menu
-            -- <c-k>: Toggle signature help
-            --
-            -- See :h blink-cmp-config-keymap for defining your own keymap
-            preset = 'enter',
-            -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-            --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+            ['<CR>'] = { 'accept', 'fallback' },
+            ['<C-\\>'] = { 'hide', 'fallback' },
+            ['<C-n>'] = { 'select_next', 'show' },
+            ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+            ['<C-p>'] = { 'select_prev' },
+            ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
         },
-
         appearance = {
             -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
             -- Adjusts spacing to ensure icons are aligned
-            kind_icons = require('icons').symbol_kinds,
             nerd_font_variant = 'mono',
         },
 
@@ -52,11 +36,21 @@ return { -- Autocompletion
             -- By default, you may press `<c-space>` to show the documentation.
             -- Optionally, set `auto_show = true` to show the documentation after a delay.
             documentation = { auto_show = false },
+            menu = {
+                scrollbar = false,
+                draw = {
+                    gap = 2,
+                    columns = {
+                        { 'kind_icon', 'kind', gap = 1 },
+                        { 'label', 'label_description', gap = 1 },
+                    },
+                },
+            },
         },
         snippets = { preset = 'luasnip' },
 
         sources = {
-            default = { 'lsp', 'path', 'snippets', 'lazydev' },
+            default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
             providers = {
                 lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
             },
